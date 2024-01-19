f64fd011e1074664ede7d2f2e5f7615d6bb88d3e
rjam117 <156855400+rjam117@users.noreply.github.com>
2024-01-19 18:10:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ function getMonday(d) {
 }
 
 function getWeekNumber(d) {
-    // Existing getWeekNumber function here
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+    return weekNo;
 }
 
 function shuffleArray(array) {
